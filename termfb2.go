cf86b8649e13b84e41df90132946da3d3be37907
Vladimir Markelov <vmatroskin@gmail.com>
2022-09-08 16:24:03 -0700

    Update code to be compatible with the latest CLUI library

--- termfb2.go
+++ termfb2.go
@@ -51,13 +51,13 @@ func createView(controls *ControlList, conf *cf.Config) {
 	controls.mainWindow = ui.AddWindow(0, 0, 22, 7, "TermFB2")
 	controls.mainWindow.SetPack(ui.Vertical)
 
-	controls.mainWindow.OnKeyDown(func(ev ui.Event) bool {
+	controls.mainWindow.OnKeyDown(func(ev ui.Event, data interface {}) bool {
 		if ev.Key == term.KeyF2 && conf.UseDb {
 			createBookListDialog(controls, conf)
 			return true
 		}
 		return false
-	})
+	}, nil)
 	controls.reader = ui.CreateTextReader(controls.mainWindow, 45, 24, 1)
 	controls.reader.SetTextColor(conf.TextColor)
 	controls.reader.SetBackColor(conf.BackColor)
@@ -191,7 +191,7 @@ func createBookListDialog(controls *ControlList, conf *cf.Config) {
 	// override OnKeyDown to support incremental search and
 	// opening selected book by pressing Enter
 	// Escape closes the dialog without doing anything
-	controls.bookListWindow.OnKeyDown(func(ev ui.Event) bool {
+	controls.bookListWindow.OnKeyDown(func(ev ui.Event, data interface {}) bool {
 		if ev.Ch != 0 {
 			filter := conf.DbDriver.Filter() + string(ev.Ch)
 			conf.DbDriver.SetFilter(filter)
@@ -228,7 +228,7 @@ func createBookListDialog(controls *ControlList, conf *cf.Config) {
 			return true
 		}
 		return false
-	})
+	}, nil)
 
 	// without overriding this function TableView shows empty values
 	controls.bookTable.OnDrawCell(func(info *ui.ColumnDrawInfo) {
